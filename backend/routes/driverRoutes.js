c019d0c21d5ae7b5a743e818a721e94d94ab5b62
TechJackk <techjack2815@gmail.com>
2025-05-13 18:51:57 +0530

    All complete except ui enhancement

--- backend/routes/driverRoutes.js
+++ backend/routes/driverRoutes.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const driverController = require("../controllers/driverController");
 const Bus = require("../models/Bus");
+const { verifyToken } = require("../middleware/authMiddleware"); // ✅ Correct named import
 
 // Route to fetch buses assigned to the driver
 router.get("/dBuses", driverController.getAssignedBuses);
 
 //seat vacancy
-router.patch("/updateSeats/:busId", async (req, res) => {
+router.patch("/updateSeats/:busId", verifyToken ,async (req, res) => {
   const { vacancy } = req.body;
 
   try {
